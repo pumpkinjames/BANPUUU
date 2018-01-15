0427ac01c469d29ee9ff12f0d7b952bd8bd309f2
pumpkinjames <33778894+pumpkinjames@users.noreply.github.com>
2018-01-15 13:51:28 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'uQhVag2wDH9/GQBiaDRW9bBjExahZxxnt09LHFUPrQ17SH06/a6iQHFcSoqky3SzCpbxFClQELgmxVaIM/gvJQJtxHxuYc9HqFeZ97yhWW8JabU+/QY9y2HUryQ4watEcWROiMGMBnqIlpYIec5cmAdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U5aa701846acba2c2204a4009b82a5f60',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
